7687b6887dab5bfca63987bb9ff960c66f205e28
Mostafa Neamatalla <34992292+Mostafa-HEH@users.noreply.github.com>
2024-10-17 06:58:58 +0400

    Time: 65 ms (10.53%) | Memory: 51.7 MB (42.11%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.ts
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.ts
@@ -1,11 +1,18 @@
 function getMinDistance(nums: number[], target: number, start: number): number {
-    for (let distance = 0; distance < nums.length; distance++) {
-        if (start + distance < nums.length && nums[start + distance] === target) {
-            return distance;
+    let left = start;
+    let right = start;
+
+    while (left >= 0 || right < nums.length) {
+        if (left >= 0 && nums[left] === target) {
+            return Math.abs(left - start);
         }
-        if (start - distance >= 0 && nums[start - distance] === target) {
-            return distance;
+        if (right < nums.length && nums[right] === target) {
+            return Math.abs(right - start);
         }
+
+        left--;
+        right++;
     }
+
     return -1;
 }

d7de789c94df1417a6324e45d52234cd96e6ce51
Mostafa Neamatalla <34992292+Mostafa-HEH@users.noreply.github.com>
2025-05-27 06:14:26 +0400

    Time: 0 ms (100.00%) | Memory: 54.3 MB (92.59%) - LeetSync

--- 899-binary-gap/binary-gap.ts
+++ 899-binary-gap/binary-gap.ts
@@ -1,26 +1,16 @@
 function binaryGap(n: number): number {
-  let distances = [];
-
   const binaryNum = (n >>> 0).toString(2);
 
-   // o(n)
-  for (let i = 0; i < binaryNum.length; i++) {
-    if (binaryNum[i] === "1") {
-      distances.push(i);
+  let zeroCount = 0;
+  let maxLen = 0;
+
+  for (const bit of binaryNum) {
+    if (bit === "0") zeroCount++;
+    else if (bit === "1") {
+      if (zeroCount > maxLen) maxLen = zeroCount;
+        zeroCount = 1;
     }
   }
 
-  if (distances.length <= 0) return 0;
-
-  let prev = 0;
-  let high = 0;
-  // o(n)
-  distances.forEach((x) => {
-    if (x - prev > high) {
-      high = x - prev;
-    }
-    prev = x;
-  });
-
-  return high;
+  return maxLen;
 }
